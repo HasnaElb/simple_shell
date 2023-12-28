3f6adc499898d61cb0c6129fb4824946b4b626b3
Hasna <hasna.elboukhari.neuro@gmail.com>
2023-12-28 02:27:29 -0800

    file corrected

--- hsh.c
+++ hsh.c
@@ -7,46 +7,44 @@
  */
 int hsh(info_t *info, char **av)
 {
-        ssize_t r = 0;
-        int builtin_ret = 0;
+	ssize_t r = 0;
+	int builtin_ret = 0;
 
-        while (r != -1 && builtin_ret != -2)
-        {
-                clear_info(info);
-                if (interactive(info))
-                        _puts("$ ");
-                _eputchar((BUF_FLUSH);
-                r = get_input(info);
-                if (r != -1)
-		{
-                        set_info(info, av);
-                        builtin_ret = find_builtin(info);
-                        if (builtin_ret == -1)
-                                find_cmd(info);
-                }       
-                else if (interactive(info))
-			_putchar('\n');
-                free_info(info, 0);
-        }       
-        write_history(info);
-        free_info(info, 1);
-        if (!interactive(info) && info->status)
-                exit(info->status);
-        if (builtin_ret ==-2)
+	while (r != -1 && builtin_ret != -2)
 	{
-		if (info->err_num == -1)
-			exit(info->status);
-		exit(info->err_num);
-	}
-	return (builtin_ret);
+		clear_info(info);
+		if (interactive(info))
+			_puts("$ ");
+		_eputchar((BUF_FLUSH);
+				r = get_input(info);
+				if (r != -1)
+				set_info(info, av);
+				builtin_ret = find_builtin(info);
+				if (builtin_ret == -1)
+				find_cmd(info);
+				else if (interactive(info))
+				_putchar('\n');
+				free_info(info, 0);
+				}
+				write_history(info);
+				free_info(info, 1);
+				if (!interactive(info) && info->status)
+				exit(info->status);
+				if (builtin_ret == -2)
+				{
+				if (info->err_num == -1)
+				exit(info->status);
+				exit(info->err_num);
+				}
+				return (builtin_ret);
 }
 /**
  * find_builtin - finds a builtin command
  * @info: the parameter & return info struct
  * Return: -1 if builtin not found,
- * 	   0 if builtin executed successfully,
- * 	   1 if builtin found but not successful,
- * 	   -2 if builtin signals exit()
+ * 0 if builtin executed successfully,
+ * 1 if builtin found but not successful,
+ * -2 if builtin signals exit()
  */
 int find_builtin(info_t *info)
 {
@@ -62,7 +60,7 @@ int find_builtin(info_t *info)
 		("alias", _myalias),
 		(NULL, NULL)
 	};
-	
+
 	for (i = 0; builtintbl[i].type; i++)
 		if (_strcmp(info->argv[0], builtintbl[i].type) == 0)
 		{
